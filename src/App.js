0b48ae196fd8ce4a180bee2d83d147f5a4371af9
drewatienza <atienza.drew@gmail.com>
2020-10-24 11:11:30 -0700

    added unstyled slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Palette from './components/Palette';
 import seedColors from './components/seedColors';
+import { generatePalette } from './components/colorHelpers';
 
-function App() {
-  return (
-    <div>
-      <Palette {...seedColors[4]}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Palette palette={generatePalette(seedColors[4])}/>
+      </div>
+    );
+  }
 }
 
 export default App;
