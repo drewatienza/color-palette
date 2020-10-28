f076b20950ae47089c80af11cf9c77bed2b99eeb
drewatienza <atienza.drew@gmail.com>
2020-10-28 08:31:40 -0700

    fixed palette route

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import seedColors from './components/seedColors';
 import { generatePalette } from './components/colorHelpers';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(palette => (
+      palette.id === id
+    ));
+  }
+
   render() {
     return (
       <Switch>
         <Route exact path='/' render={() => <h1>PALETTE LIST GOES HERE</h1>} />
-        <Route exact path='/palette/:id' render={() => <h1>INDIVIDUAL PALETTE</h1>} />
+        <Route 
+          exact 
+          path='/palette/:id' 
+          render={routeProps => (
+            <Palette 
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )} 
+        />
         {/* <div>
           <Palette palette={generatePalette(seedColors[4])}/>
         </div> */}
